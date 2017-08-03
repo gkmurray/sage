6a46ad57dbde7e375e366f2ba048bf3969d63823
Greg Murray <greg@aorweb.ca>
2017-08-03 12:02:19 -0300

    Use foundation module syntax
    Using the new es6 module syntax that came with Foundation 6.4 gives us better control over which js gets bundled.

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,11 +1,7 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
-
-    /*
-	*	INITALIZE FOUNDATION
-	********************************************************************************/
-	$(document).foundation();
+    $(document).foundation();
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
